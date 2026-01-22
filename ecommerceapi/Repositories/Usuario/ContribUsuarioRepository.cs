d8c4b2ab3d7e245a0be7bb9affee2a63e77bb587
Mateus Cizeski Kogl <mateuscizeski@gmail.com>
2026-01-22 19:39:44 -0300

    Implementing controllers and a new library, dapper.contrib

---
+++ ecommerceapi/Repositories/Usuario/ContribUsuarioRepository.cs
@@ -0,0 +1,32 @@
+﻿using ecommerceapi.Models;
+
+namespace ecommerceapi.Repositories
+{
+    public class ContribUsuarioRepository : IUsuarioRepository
+    {
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Usuario> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Usuario Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
